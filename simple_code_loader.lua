8805a65b7c5ac818b4596a1a45d06a604c0734a3
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-08-29 20:45:15 +0400

    Create simple_code_loader.lua

---
+++ simple_code_loader.lua
@@ -0,0 +1,15 @@
+local folder = "/"
+local function addFile( path, client, server )
+		local fullPath = folder .. path .. ".lua"
+		if (SERVER) then
+				if (client) then
+						AddCSLuaFile( fullPath )
+				end
+
+				if (server) then
+						include( fullPath )
+				end
+		elseif (client) then
+				include( fullPath )
+		end
+end
