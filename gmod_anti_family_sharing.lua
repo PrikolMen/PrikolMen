a66b813fa0f404d8af171a2680edd8a6744a41b7
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2023-03-07 13:47:02 +0400

    Delete gmod_anti_family_sharing.lua

--- gmod_anti_family_sharing.lua
+++
@@ -1,8 +0,0 @@
-local cvar = CreateConVar('allow_family_sharing', '0', FCVAR_ARCHIVE, ' - Allows connecting players with family shared Garry\'s Mod copy.', 0, 1 )
-hook.Add('PlayerInitialSpawn', 'Simple Anti-Family Sharing', function( ply )
-    if ply:IsBot() or ply:IsListenServerHost() then return end
-    if cvar:GetBool() then
-        if (ply:OwnerSteamID64() ~= ply:SteamID64()) then return end
-        ply:Kick( 'Family sharing restricted!' )
-    end
-end)
